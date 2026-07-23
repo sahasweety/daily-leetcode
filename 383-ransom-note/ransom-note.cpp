e991afb250661da3649326d387637e96d2a1f447
sahasweety <sweetysaha729@gmail.com>
2026-07-24 00:44:46 +0530

    Time: 8 ms (14.89%) | Memory: 12 MB (29.99%) - LeetSync

---
+++ 383-ransom-note/ransom-note.cpp
@@ -0,0 +1,18 @@
+//google question
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char, int> freq;
+
+        for (char ch : magazine)
+            freq[ch]++;
+
+        for (char ch : ransomNote) {
+            freq[ch]--;
+            if (freq[ch] < 0)
+                return false;
+        }
+
+        return true;
+    }
+};
