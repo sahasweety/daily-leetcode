10afc58d83ebf6cf5a2ddd73ba23fdebe3847a20
sahasweety <sweetysaha729@gmail.com>
2026-07-29 23:26:51 +0530

    Time: 15 ms (95.43%) | Memory: 70.6 MB (48.53%) - LeetSync

---
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -0,0 +1,67 @@
+/***class Solution {
+public:
+    int minDays(vector<int>& bloomDay, int m, int k) {
+        
+    }
+};*/
+class Solution {
+public:
+
+    bool canMake(vector<int>& bloomDay, int m, int k, int day) {
+
+        int flowers = 0;
+        int bouquets = 0;
+
+        for (int bloom : bloomDay) {
+
+            if (bloom <= day) {
+
+                flowers++;
+
+                if (flowers == k) {
+
+                    bouquets++;
+                    flowers = 0;
+                }
+            }
+            else {
+
+                flowers = 0;
+            }
+        }
+
+        return bouquets >= m;
+    }
+
+    int minDays(vector<int>& bloomDay, int m, int k) {
+
+        long long need = 1LL * m * k;
+
+        if (need > bloomDay.size())
+            return -1;
+
+        int low = *min_element(bloomDay.begin(), bloomDay.end());
+
+        int high = *max_element(bloomDay.begin(), bloomDay.end());
+
+        int ans = -1;
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (canMake(bloomDay, m, k, mid)) {
+
+                ans = mid;
+
+                high = mid - 1;
+            }
+            else {
+
+                low = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+};
