bb8a647561fc8e423b9c7bf7e5cba12877eac596
sahasweety <sweetysaha729@gmail.com>
2026-07-30 23:55:01 +0530

    Time: 0 ms (100.00%) | Memory: 22.6 MB (47.49%) - LeetSync

---
+++ 275-h-index-ii/h-index-ii.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+
+class Solution {
+public:
+    int hIndex(std::vector<int>& citations) {
+        int n = citations.size();
+        int left = 0, right = n - 1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            // If the number of citations is greater than or equal to 
+            // the number of papers having at least `citations[mid]` citations
+            if (citations[mid] >= n - mid) {
+                right = mid - 1; // Try to find a larger h-index on the left
+            } else {
+                left = mid + 1;  // Look for a larger citation value on the right
+            }
+        }
+        
+        // `left` points to the first index where citations[mid] >= n - mid
+        // Therefore, the total number of valid papers is n - left
+        return n - left;
+    }
+};
