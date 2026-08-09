4e946235c70c6782ce77f65ce8b98985b6bf6571
sahasweety <sweetysaha729@gmail.com>
2026-08-09 18:27:27 +0530

    Time: 3 ms (54.33%) | Memory: 11 MB (59.72%) - LeetSync

---
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+       unordered_map<char,int>mp;
+       for(int i =0; i <s.size();i++){
+        mp[s[i]]++;
+       } 
+       priority_queue<pair<int , char>>maxH;
+
+       for(auto it : mp){
+        int freq = it.second;
+        char element = it.first;
+
+        maxH.push({freq,element});
+       }
+       string ans = "";
+
+       while(maxH.size() > 0){
+        int freq= maxH.top().first;
+        char element = maxH.top().second;
+
+        for(int i=0; i < freq ; i++){
+            ans += element;
+        }
+        maxH.pop();
+       }
+       return ans;
+    }
+};
